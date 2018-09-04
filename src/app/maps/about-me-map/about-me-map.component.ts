f3afd5425e8119ef2bd5eb6c2702ddb4ea957925
Brian Brown <brbrowngeo@gmail.com>
2018-09-03 21:02:45 -0400

    new map

--- src/app/maps/about-me-map/about-me-map.component.ts
+++ src/app/maps/about-me-map/about-me-map.component.ts
@@ -1,25 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import * as d3Fetch from 'd3-fetch';
 import * as topojson from 'topojson-client';
 import { MapUtilities } from '../map-utilities';
 
 @Component({
   selector: 'about-me-map',
   templateUrl: './about-me-map.component.html',
-  styleUrls: ['./about-me-map.component.css']
+  styleUrls: ['./about-me-map.component.scss']
 })
 export class AboutMeMapComponent implements OnInit {
   //  map components
   map;
-  data: { states: string } = {
-    states: "/assets/page/notfound/states.topo.json",
+  data: {[key: string]: string } = {
+    richmond: "/assets/page/about/richmond.topo.json",
+    places: "/assets/page/about/places.topo.json"
   };
-  width: number = 500;
-  height: number = 340;
+  width: number = 500 * 1.5;
+  height: number = 340 * 1.5;
   svg;
   path;
   g;
   projection;
+  popup: string = "home";
   constructor(
     public mu: MapUtilities
   ) { }
@@ -34,52 +37,54 @@ export class AboutMeMapComponent implements OnInit {
       .translate([0, 0]);
     this.path = d3.geoPath(this.projection);
     this.g = this.svg.append("g")
-      .attr("class", "boundary")
     this.drawMap();
+
   }
 
   drawMap() {
-    d3.json(this.data.states, (error, us) => {
-      if (error) throw error;
+    Promise.all([
+      d3Fetch.json(this.data.richmond),
+      d3Fetch.json(this.data.places)
+    ]
+    ).then(([richmondJson, placesJson]) => {
 
-      let statesArray = topojson.feature(us, us["objects"].states).features;
+      let richmond = topojson.feature(richmondJson, richmondJson["objects"].richmond).features[0];
+      let richmondPlaces = topojson.feature(placesJson, placesJson["objects"].richmond).features;
 
-      let states = this.mu.shuffleArray(statesArray);
-      let state = states.shift();
-      run.call(this);
-      function run() {
-        // Reset projection before redrawing
-        this.projection
-          .scale(1)
-          .translate([0, 0]);
-        this.mu.zoomIn(this.projection, this.path.bounds(state), this.width, this.height);
-        var delay = 0;
-        var duration = 0;
+      let delay = 0;
+      let duration = 0;
 
-        this.g.append("path")
-          .datum(state)
-          .attr("d", this.path)
-          .each(function (d) { duration = (d.length = this.getTotalLength()) * 3 })
-          .style("stroke-width", "2px")
-          .style("stroke-dasharray", function (d) { return `0,${d.length}`; })
-          .transition()
-            .delay((d, i) => { var delay1 = delay; delay += duration; return delay1; })
-            .duration(d => { return duration; })
-            .ease(d3.easeLinear)
-            .on("start", function (d) { this.style.stroke = "#303e4d"; })
-            .style("stroke-dasharray", function (d) { return `${d.length},${d.length}`; })
-          .transition()
-            .duration(1200)
-            .style("stroke-opacity", 0)
-            .attr("transform", "")
-            .remove()
-            .on("end", run.bind(this), this);
+      // Reset projection before redrawing
+      this.projection
+        .scale(1)
+        .translate([0, 0]);
+      this.mu.zoomIn(this.projection, this.path.bounds(richmond), this.width, this.height);
+      this.g.append("path")
+        .datum(richmond)
+        .attr("d", this.path)
+        .each(function (d) { duration = (d.length = this.getTotalLength()) * 2 })
+        .style("stroke-dasharray", (d) => `0,${d.length}`)
+        .attr("class", "map-boundary")
+        .transition()
+          .delay((d, i) => { var delay1 = delay; delay += duration; return delay1; })
+          .duration(d => { return duration; })
+          .ease(d3.easeLinear)
+          .style("stroke-dasharray", (d) => `${d.length},${d.length}`);
 
-        if (states.length === 0) {
-          states = this.shuffleArray(topojson.feature(us, us["objects"].states).features);
-        }
-        state = states.shift();
-      }
+      console.log(richmondPlaces)
+      let points = this.g.selectAll("circle")
+      .data(richmondPlaces).enter()
+      .append("circle")
+        .attr("cx", (d) => this.projection(d.geometry.coordinates)[0])
+        .attr("cy", (d) => this.projection(d.geometry.coordinates)[1])
+        .attr("r", "6px")
+        .attr("class", "map-point")
+        .on("click", (e) => this.popup = e.properties.location)
+
+      // bind class off property, then use css to style
+      points.each(function(d:any) {
+        this.classList.add(`map-point--${d.properties.location}`);
+      });
     });
   }
 
