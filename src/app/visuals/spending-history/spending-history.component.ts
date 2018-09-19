e74c11f4f340322b8a57b34e50e7cdb1c8a3d238
Brian Brown <brbrowngeo@gmail.com>
2018-09-18 20:36:46 -0400

    base spending graph

--- src/app/visuals/spending-history/spending-history.component.ts
+++ src/app/visuals/spending-history/spending-history.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
 import * as d3Fetch from 'd3-fetch';
 import * as d3Selection from 'd3-selection';
+import * as d3Time from 'd3-time';
+import * as d3Scale from 'd3-scale';
+import * as d3TimeFormat from 'd3-time-format';
+import * as d3ScaleChromatic from 'd3-scale-chromatic';
 import 'd3-transition';
 import { forkJoin, from, Subject } from 'rxjs';
 import { throttleTime } from 'rxjs/operators';
@@ -18,6 +22,8 @@ export class SpendingHistoryComponent implements OnInit {
 
   width: number;
   height: number;
+  margin: number = 30;
+  cellSize: number = 18;
   svg;
   changeSize = new Subject();
   spendingUrl: string = `${environment.budget}/api/public/finance/transactions-per-day`;
@@ -40,17 +46,43 @@ export class SpendingHistoryComponent implements OnInit {
 
   draw() {
     
-    this.width = this.graph.nativeElement.clientWidth;
-    // this.height = this.width * .7
-    this.height = Math.ceil((this.width * 9) / 16);
+    this.width = this.graph.nativeElement.clientWidth - this.margin;
+    this.height = this.cellSize * 10 - this.margin
+    // this.height = Math.ceil((this.width * 9) / 16);
 
     this.svg = d3Selection.select(this.graph.nativeElement).append("svg")
       .attr("width", this.width)
       .attr("height", this.height);
     
-    from(d3Fetch.json(this.spendingUrl))
+    from<Array<{transactionCount: number, date:string}>>(d3Fetch.json(this.spendingUrl))
       .subscribe(data => {
-        console.log(data);
+        const formatTime = d3TimeFormat.timeFormat("%Y-%m-%d");
+
+        const today = d3Time.timeMonth.offset(new Date(), -3);
+        const yearAgo = d3Time.timeYear.offset(today, -1);
+        const days = d3Time.timeDay.range(yearAgo, today);
+        let recentTransactions = data.filter(d => d.date > formatTime(yearAgo));
+        let max = recentTransactions.reduce((a,b) => a.transactionCount > b.transactionCount ? a : b).transactionCount;
+        let colors = d3ScaleChromatic.schemeGreens[9];
+
+        let rect = this.svg.append("g")
+            .attr("stroke", "#ccc")
+          .selectAll("rect")
+          .data(days)
+          .enter().append("rect")
+            // TODO data joins the d3 way
+            .attr("fill", (d) => {
+              let transaction = recentTransactions.find(r => r.date == formatTime(d))
+              return colors[transaction != null ? transaction.transactionCount : 0];
+            })
+            .attr("width", this.cellSize)
+            .attr("height", this.cellSize)
+            .attr("x", (d) => d3Time.timeWeek.count(yearAgo, d) * this.cellSize) 
+            .attr("y", (d) => d.getDay() * this.cellSize)
+            .datum(d3TimeFormat.timeFormat("%Y-%m-%d"));
+
+
+
     });
 
   }
