df6ffcb15325685e4e1a63ad3e88311592cfcd2f
Brian Brown <brbrowngeo@gmail.com>
2019-09-08 11:20:31 -0400

    Remove jenkins build

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x #echo on
-
-echo "Building package"
-node_modules/@angular/cli/bin/ng build --prod --aot 
-echo "Clearing previous builds"
-rm -f /var/www/bgeo/*
-echo "Syncing package"
-rsync -a ./dist/ /var/www/bgeo/
